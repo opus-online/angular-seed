ecefd51d313a70944e20e176b6cf2414af06f790
Peeter <peeter.tomberg@gmail.com>
2016-03-11 23:24:49 +0200

    Style fixes

--- src/core/registry.js
+++ src/core/registry.js
@@ -1,11 +1,12 @@
 /**
  * Creates a registry that builds the name from folders
+ * @param prettyTypeName - The pretty type name, only used for debugging purposes
  * @param pathTransformers Functions that take the path and transform it into a name
  * @param valueValidators Functions that check the values against best practices
  * @param registration Callback that will be invoked with the application, the transformed name and the value of the file
  * @returns {Function}
  */
-export const createFolderNameRegistry = (type, pathTransformers = [], valueValidators = [], registration) => {
+export const createFolderNameRegistry = (prettyTypeName, pathTransformers = [], valueValidators = [], registration) => {
     return (context, { application, warn, debug, info, error }) => {
 
         context
@@ -17,7 +18,7 @@ export const createFolderNameRegistry = (type, pathTransformers = [], valueValid
 
                 valueValidators.forEach((validator) => validator({ path, name : transformedName, value }, { warn, debug, info, error}));
 
-                debug(`Registering '${type}' from '${path}' with transformed name '${transformedName}'`);
+                debug(`Registering '${prettyTypeName}' from '${path}' with transformed name '${transformedName}'`);
                 registration(application, transformedName, value);
             });
     };
