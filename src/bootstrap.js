7d99219b8a4a338d7fdde6e43222f9976071c5d8
Peeter <peeter.tomberg@gmail.com>
2016-08-16 17:06:44 +0300

    Reduced code a bit. Reworked groups a bit.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,42 +1,23 @@
 import { registerComponents, registerConfigs, registerRuns, registerLayouts, registerStates, registerConstants, registerEnums,
     registerProviders, registerResources, registerFactories, registerServices, registerFilters } from 'opus-angular-seed-core';
 
-const startRegistering = () => {
-    if (SEED_CORE.ENV.NODE_ENV === 'development') {
-        console.groupCollapsed('opus-angular-seed registering application elements');
-    }
+const noop = () => {};
+const logger = SEED_CORE.ENV.NODE_ENV === 'development' ? console : {
+    group: noop,
+    groupCollapsed: noop,
+    groupEnd: noop,
+    error: noop,
+    info: noop,
+    warn: noop,
+    debug: noop
 };
-const endRegistering = () => {
-    if (SEED_CORE.ENV.NODE_ENV === 'development') {
-        console.groupEnd();
-    }
-};
-const error = (...args) => {
-    if (SEED_CORE.ENV.NODE_ENV === 'development') {
-        console.error.apply(console, args);
-    }
-};
-const info = (...args) => {
-    if (SEED_CORE.ENV.NODE_ENV === 'development') {
-        console.info.apply(console, args);
-    }
-};
-const warn = (...args) => {
-    if (SEED_CORE.ENV.NODE_ENV === 'development') {
-        console.warn.apply(console, args);
-    }
-};
-const debug = (...args) => {
-    if (SEED_CORE.ENV.NODE_ENV === 'development') {
-        console.debug.apply(console, args);
-    }
-};
+const { info, warn, debug, error } = logger;
 
 const prefix = '';
 const application = angular.module('app', SEED_CORE.DEPENDENCIES.ANGULAR);
 const options = { application, prefix, info, warn, debug, error };
 
-startRegistering();
+logger.groupCollapsed('[opus-angular-seed] Registration phase');
 
 /**
  * Register configuration stuff
@@ -61,7 +42,8 @@ registerEnums(require.context('./enums', true, /index.js/), options);
  */
 registerRuns(require.context('./runs', true, /index.js/), options);
 
-endRegistering();
+logger.groupEnd();
+logger.group('[opus-angular-seed] Bootstrapping phase');
 
 try {
     angular.bootstrap(document, [application.name]);
@@ -73,3 +55,4 @@ catch (e) {
     info(`Vendors (package.json field 'vendors'): ${SEED_CORE.VENDORS}`);
     throw e;
 }
+logger.groupEnd();
