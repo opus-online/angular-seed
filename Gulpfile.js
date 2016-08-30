8f48336fc654a10d7d8d22bc19a18e0e227a7c95
Peeter <peeter.tomberg@gmail.com>
2016-08-30 12:50:37 +0300

    Fixed path problem for translations

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const extractTranslate = require('gulp-angular-translate-extractor');
 
 gulp.task('extract', () => {
-    const i18nsrc = './src/app/**/*.*';
+    const i18nsrc = './src/**/*.*';
     const i18ndest = './www/languages';
     return gulp.src(i18nsrc)
         .pipe(extractTranslate({
